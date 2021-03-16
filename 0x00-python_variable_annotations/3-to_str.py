93ef89f47a946facc709794f7e9248baecff2eca
matxa <42968545+matxa@users.noreply.github.com>
2021-03-15 22:18:12 -0400

    flaot to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function to_str that
+takes a float n as argument and returns the
+string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """return string representation of a flaot"""
+    return str(n)
