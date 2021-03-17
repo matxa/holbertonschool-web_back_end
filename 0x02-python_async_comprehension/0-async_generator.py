7fb7e3d8882b75cc5afde6754e7ba25c90a395ab
matxa <42968545+matxa@users.noreply.github.com>
2021-03-17 12:26:58 -0400

    [ FIX ] type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,10 +6,10 @@ then yield a random number between 0 and 10. Use the random module.
 """
 import asyncio
 import random
-import typing
+from typing import Generator
 
 
-async def async_generator() -> typing.Generator[float]:
+async def async_generator() -> Generator[float, None, None]:
     """async generator
 
     Returns
