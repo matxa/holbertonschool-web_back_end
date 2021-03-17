ae86443e6eca5daed0002033b5da3cb7c589bbb6
matxa <42968545+matxa@users.noreply.github.com>
2021-03-16 23:23:21 -0400

    fix documentation

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -9,6 +9,16 @@ import asyncio
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """return Task"""
+    """Takes in an async function
+    then creates and returns an asyncio Task
+
+    Parameters
+    ----------
+    max_delay: int
+
+    Returns
+    -------
+    asyncio.Task
+    """
     wait_random = __import__('0-basic_async_syntax').wait_random
     return asyncio.create_task(wait_random(max_delay))
