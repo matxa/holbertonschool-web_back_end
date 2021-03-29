b996aa430a3bad8b7ed8d6f5c30b8788423fe0a9
matxa <42968545+matxa@users.noreply.github.com>
2021-03-29 13:47:50 -0400

    test

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ import re
 
 
 def filter_datum(
-     fields: List, redaction: str, message: str, separator: str) -> str:
+     fields: List[str], redaction: str, message: str, separator: str) -> str:
     """hash values"""
     for field in fields:
         message = re.sub(r"(?<={}=)(.*?(?={}))".format(
