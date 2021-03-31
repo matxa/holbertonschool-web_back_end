0dd7e553c07bc327711ad44ad0d4b26aea4fb967
matxa <42968545+matxa@users.noreply.github.com>
2021-03-31 12:16:37 -0400

    create Forbidden [ 403 ] error

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,10 @@ def unauthorized() -> str:
     """ Testing 401 ERROR
     """
     return abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """ Testing 403 ERROR
+    """
+    return abort(403)
