5828e9606468aab6f8f7399f51c355cea2c63e40
matxa <42968545+matxa@users.noreply.github.com>
2021-04-01 15:42:01 -0400

    extract base64 from header

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 """ basic_auth module
 """
-from .auth import Auth
+from auth import Auth
 
 
 class BasicAuth(Auth):
     """ BasicAuth class
     """
     pass
+
+    def extract_base64_authorization_header(
+         self, authorization_header: str) -> str:
+        """ Ectract base64
+        """
+        if authorization_header is None or type(authorization_header) != str\
+           or authorization_header.startswith("Basic ") is False:
+            return None
+        else:
+            return authorization_header.split()[1]
