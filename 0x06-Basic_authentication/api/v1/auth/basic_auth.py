37f87793624e403c85dfa1eca90b4066a15b1164
matxa <42968545+matxa@users.noreply.github.com>
2021-04-01 17:24:28 -0400

    decoded base64

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,16 @@ class BasicAuth(Auth):
                 base64_authorization_header).decode('utf-8')
         except Exception as e:
             return None
+
+    def extract_user_credentials(
+         self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Decode base64 with extra credentials
+        """
+        if decoded_base64_authorization_header is None or\
+           type(decoded_base64_authorization_header) != str or\
+           ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        return (
+            decoded_base64_authorization_header.split(':')[0],
+            decoded_base64_authorization_header.split(':')[1]
+            )
