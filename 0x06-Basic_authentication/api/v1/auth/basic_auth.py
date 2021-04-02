195739c805a1076b73c0399476e74abb8671b8ca
matxa <42968545+matxa@users.noreply.github.com>
2021-04-01 23:18:34 -0400

    current user

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -61,3 +61,12 @@ class BasicAuth(Auth):
             return None
         else:
             return user[0]
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current user
+        """
+        auth_header = self.authorization_header(request)
+        self.extract_base64_authorization_header(auth_header)
+        self.decode_base64_authorization_header(auth_header)
+        credentials = self.extract_user_credentials("")
+        self.user_object_from_credentials(credentials)
