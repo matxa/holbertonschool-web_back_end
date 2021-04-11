c04022f144099541dc3a28731f3fe75a7dfa90c8
matxa <42968545+matxa@users.noreply.github.com>
2021-04-11 15:39:21 -0400

    get user_id by session_id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -21,3 +21,10 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ user id for sesseion id
+        """
+        if session_id is None or type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
