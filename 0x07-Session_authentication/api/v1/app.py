1cb6dca0f0fde1b2c11aff1f34eec51137b42aca
matxa <42968545+matxa@users.noreply.github.com>
2021-04-11 15:12:58 -0400

    create empty class SessionAuth

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -22,6 +22,9 @@ if auth:
     elif auth == 'basic_auth':
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
+    elif auth == 'session_auth':
+        from api.v1.auth.session_auth import SessionAuth
+        auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Session Auth module
+"""
+from .auth import Auth
+import base64
+from models.user import User
+from typing import TypeVar
+
+
+class SessionAuth(Auth):
+    """ Session Auth class
+    """
+    pass
