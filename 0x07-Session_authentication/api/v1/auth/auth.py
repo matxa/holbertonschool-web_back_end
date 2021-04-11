e51401d05803abbd549b82833e8bd31fa8b16c5a
matxa <42968545+matxa@users.noreply.github.com>
2021-04-11 15:59:35 -0400

    get cookies

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -35,3 +36,12 @@ class Auth:
         """ current user
         """
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """ session cookie
+        """
+        if request is None:
+            return None
+
+        cookie_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(cookie_name)
