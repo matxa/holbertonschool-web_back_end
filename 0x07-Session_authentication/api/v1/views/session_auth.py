fc1c0516c9dfcc59af2d78867943a50b96e57f11
matxa <42968545+matxa@users.noreply.github.com>
2021-04-11 21:25:43 -0400

    add endpoint /auth_session/login

---
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Module of Index views
+"""
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth_session() -> str:
+    """ Auth Session
+    """
+    email = request.form.get("email")
+    pwd = request.form.get("password")
+
+    if email is None or email == "":
+        return jsonify({"error": "email missing"}), 400
+    if pwd is None or pwd == "":
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({"email": email})
+    if len(user) < 1:
+        return jsonify({"error": "no user found for this email"}), 404
+    user = user[0]
+    if user.is_valid_password(pwd) is False:
+        return jsonify({"error": "wrong password"}), 401
+
+    # IMPORT may cause circular import error
+    from api.v1.app import auth
+    cookie = os.getenv('SESSION_NAME')
+
+    res = jsonify(user.to_json())
+    res.set_cookie(cookie, auth.create_session(user.id))
+    return res
