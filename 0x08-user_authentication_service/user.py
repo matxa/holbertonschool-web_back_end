77dda22de898072d405f6543f2efd2ff367f0f65
matxa <42968545+matxa@users.noreply.github.com>
2021-04-16 08:54:34 -0400

    [ ADD ] max string length to column

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -14,7 +14,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(20), nullable=False)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
