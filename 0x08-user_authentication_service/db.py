3dbc5f23b548a08ab87af538f7a69b92e028418c
matxa <42968545+matxa@users.noreply.github.com>
2021-04-17 15:41:47 -0400

    testing old code

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -62,13 +62,10 @@ class DB:
     def update_user(self, user_id: int, **kwargs) -> None:
         """ Update user
         """
-        try:
-            user = self.find_user_by(id=user_id)
-            for key, value in kwargs.items():
-                if not hasattr(user, key):
-                    raise ValueError
-                setattr(user, key, value)
-            self._session.commit()
-            return None
-        except NoResultFound:
-            return None
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
+        return None
