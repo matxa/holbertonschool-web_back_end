0380df8dcfd585199bb30089342449018a10151c
matxa <42968545+matxa@users.noreply.github.com>
2021-04-18 10:16:51 -0400

    /users [POST] endpoint to to register user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ App module """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -13,5 +15,23 @@ def home():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register():
+    """ Register User
+    """
+    if "email" in request.form.keys() and "password" in request.form.keys():
+        try:
+            email = request.form['email']
+            pwd = request.form['password']
+            AUTH.register_user(email, pwd)
+            return jsonify({
+                "email": "{}".format(email),
+                "message": "user created"})
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"message": "missing parameters email or password"})
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
