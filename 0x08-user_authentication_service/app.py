4706074ae8f4a2c8aa160632eb22ca3a45c8af20
matxa <42968545+matxa@users.noreply.github.com>
2021-04-18 11:21:00 -0400

    create login ednpoint

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ App module """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
+from sqlalchemy.orm.exc import NoResultFound
 from auth import Auth
 
 
@@ -31,6 +32,29 @@ def register():
             return jsonify({"message": "email already registered"}), 400
 
     return jsonify({"message": "missing parameters email or password"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ Login user
+    """
+    if "email" in request.form.keys() and "password" in request.form.keys():
+        try:
+            email = request.form['email']
+            password = request.form['password']
+            if AUTH.valid_login(email, password):
+                session_id = AUTH.create_session(email)
+                response = {
+                    "email": "{}".format(email),
+                    "message": "logged in"
+                }
+                response = jsonify(response)
+                response.set_cookie("session_id", session_id)
+                return response
+            else:
+                abort(401)
+        except NoResultFound:
+            abort(401)
 
 
 if __name__ == "__main__":
