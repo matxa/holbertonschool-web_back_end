6ab3ccab148ecfaefda36d7892d97e3b54eb2346
matxa <42968545+matxa@users.noreply.github.com>
2021-04-18 18:29:39 -0400

    remove session id from user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -67,3 +67,14 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroy session
+        """
+        try:
+            user = self._db.find_user_by(user_id=user_id)
+            user.session_id = None
+            self._db.commit()
+            return None
+        except NoResultFound:
+            return None
