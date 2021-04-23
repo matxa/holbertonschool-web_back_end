bab4b30e21e951b684a3b85f7aa1f5c2a8e37e6a
matxa <42968545+matxa@users.noreply.github.com>
2021-04-22 21:59:12 -0400

    add babel and flask config

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 """ Flask App """
 from flask import Flask, render_template
-
+from flask_babel import Babel
 
 app = Flask(__name__)
+
+
+class Config:
+    """ Config class
+    """
+    LANGUAGES = ["en", "fr"]
+
+
+babel = Babel(
+    app=app,
+    default_locale=Config.LANGUAGES[0],
+    default_timezone='UTC')
+
+
+app.config.from_object(Config())
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
     """ Home endpoint
     """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
