685223b736a78c066265fd39de0acd104cec1b64
matxa <42968545+matxa@users.noreply.github.com>
2021-04-27 23:03:10 -0400

    create count_calls decorator

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,6 +3,18 @@
 import redis
 import uuid
 from typing import Union, Optional, Callable, Any
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Count how many times a method is called
+    """
+    @wraps(method)
+    def wrapper(self, *args) -> bytes:
+        key = method.__qualname__
+        count = self._redis.incr(key, 1)
+        return count
+    return wrapper
 
 
 class Cache:
@@ -14,6 +26,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Store data in Cache
         """
