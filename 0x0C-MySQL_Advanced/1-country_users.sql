63ad9e2cfc479f1ca74fa791d7b68e2ade0ae3d2
matxa <42968545+matxa@users.noreply.github.com>
2021-04-29 13:55:14 -0400

    enums

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Create users table with attributes id, email, name
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
+);
