572f05250ddeca41417ff2a16dc63c124f854ec8
matxa <42968545+matxa@users.noreply.github.com>
2021-04-29 20:45:17 -0400

    2

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,3 @@
 -- Write a SQL script that ranks country origins of bands
 -- ordered by the number of (non-unique) fans
-SELECT origin, fans FROM metal_bands ORDER BY fans NOT UNIQUE;
+SELECT origin, fans FROM metal_bands ORDER BY fans;
