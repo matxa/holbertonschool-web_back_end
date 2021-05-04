741df0e96a78a5260730244b983156c8540d0fd5
matxa <42968545+matxa@users.noreply.github.com>
2021-05-04 10:07:09 -0400

    ass file guard

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -2,14 +2,16 @@
 """ provides some stats about Nginx logs stored in MongoDB """
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx = client.logs.nginx
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-print("{} logs".format(nginx.count_documents({})))
-print("Methods:")
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-for request_type in method:
-    print("\tmethod {}: {}".format(
-        request_type, nginx.count_documents({"method": request_type})))
-print("{} status check".format(nginx.count_documents({"path": "/status"})))
+    print("{} logs".format(nginx.count_documents({})))
+    print("Methods:")
+
+    for request_type in method:
+        print("\tmethod {}: {}".format(
+            request_type, nginx.count_documents({"method": request_type})))
+    print("{} status check".format(nginx.count_documents({"path": "/status"})))
