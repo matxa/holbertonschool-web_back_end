2e65f482419e2b374552f9abe78f48c92aa4028f
matxa <42968545+matxa@users.noreply.github.com>
2021-05-04 09:57:08 -0400

    change count for count_documents

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -6,7 +6,7 @@ client = MongoClient('mongodb://127.0.0.1:27017')
 nginx = client.logs.nginx
 method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-print(nginx.count_documents({}))
+print("{} logs".format(nginx.count_documents({})))
 
 for request_type in method:
     print("\tmethod {}: {}".format(
