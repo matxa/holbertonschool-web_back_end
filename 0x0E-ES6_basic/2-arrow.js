0f14ef83662760d67d27b9991ab7f4812f566221
matxa <42968545+matxa@users.noreply.github.com>
2021-05-06 12:44:21 +0000

    change arrow func

--- 0x0E-ES6_basic/2-arrow.js
+++ 0x0E-ES6_basic/2-arrow.js
@@ -2,9 +2,8 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  const add = (newNeighborhood) => {
+  this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
-  this.addNeighborhood = add;
 }
