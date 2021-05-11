c2fbc8293a4c87b5e604d8648776f24c1b9c56a8
matxa <42968545+matxa@users.noreply.github.com>
2021-05-11 16:18:30 -0400

    3

---
+++ 0x0F-ES6_promise/3-all.js
@@ -0,0 +1,6 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((output) => console.log(output[0].body, output[1].firstName, output[1].lastName)).catch(() => console.log('Signup system offline'));
+}
