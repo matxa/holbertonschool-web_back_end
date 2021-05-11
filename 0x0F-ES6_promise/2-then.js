4b73b97d2e0153e51d3df98b4fc854b35507d10b
matxa <42968545+matxa@users.noreply.github.com>
2021-05-11 15:54:14 -0400

    task 1 and 2

---
+++ 0x0F-ES6_promise/2-then.js
@@ -0,0 +1,6 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  }).catch(() => new Error());
+}
