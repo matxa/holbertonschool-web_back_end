d54fee0f1096bfd86de378113afbfbfe964170be
matxa <42968545+matxa@users.noreply.github.com>
2021-05-11 16:00:20 -0400

    use finally

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,6 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  promise.then(() => ({ status: 200, body: 'success' })).catch(() => new Error('').finally(() => {
     console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
-  }).catch(() => new Error());
+  }));
 }
