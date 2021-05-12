00c02c8c58df6b1b85e6e63fbee34edaf481a3da
matxa <42968545+matxa@users.noreply.github.com>
2021-05-12 08:55:00 -0400

    add return statment

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,9 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const uPhoto = uploadPhoto();
-  const cUser = createUser();
-  Promise.all([uPhoto, cUser])
-    .then((output) => console.log(`${output[0].body} ${output[1].firstName} ${output[1].lastName}`),
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((output) => console.log(output[0].body, output[1].firstName, output[1].lastName),
       () => console.log('Signup system offline'));
 }
