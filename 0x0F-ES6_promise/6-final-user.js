dd7be6633953b705e38ce6d2c0b40e774a65edfe
matxa <42968545+matxa@users.noreply.github.com>
2021-05-12 18:47:47 -0400

    change obj

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,6 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((output) => output);
+  const outputList = [];
+  const promise = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((outputs) => {
+      outputs.forEach((obj) => {
+        const objCopy = obj;
+        if (objCopy.status === 'rejected') {
+          [objCopy.value] = objCopy.reason.toString().split('\n');
+          delete objCopy.reason;
+        }
+        outputList.push(objCopy);
+      });
+      return outputList;
+    });
+  return promise;
 }
