7e28f41b3d19eecea02e097e93aba52f1fca658e
matxa <42968545+matxa@users.noreply.github.com>
2021-05-12 08:44:41 -0400

    errors

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,6 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((output) => console.log(`${output[0].body} ${output[1].firstName}, ${output[1].lastName}`), () => console.log('Signup system offline'));
+  const photo = uploadPhoto();
+  const user = createUser();
+  return Promise.all([photo, user])
+    .then((promises) => console.log(`${promises[0].body} ${promises[1].firstName} ${promises[1].lastName}`),
+      () => console.log('Signup system offline'));
 }
