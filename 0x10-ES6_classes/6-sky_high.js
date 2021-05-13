f753f4d2349ad426e9438f01495fac9317ac69c6
matxa <42968545+matxa@users.noreply.github.com>
2021-05-13 07:08:01 -0400

    6

---
+++ 0x10-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._sqft = sqft;
+    this._floors = floors;
+  }
+
+  get sqft() { return this._sqft; }
+
+  set sqft(sqft) { this._sqft = sqft; }
+
+  get floors() { return this._floors; }
+
+  set floors(floors) { this._floors = floors; }
+
+  static evacuationWarningMessage() {
+    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
+  }
+}
