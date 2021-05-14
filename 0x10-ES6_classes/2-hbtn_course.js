2e077aa7819d94b965616a0a6a823cbb4c008e7a
matxa <42968545+matxa@users.noreply.github.com>
2021-05-13 22:15:29 -0400

    2

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -10,13 +10,13 @@ export default class HolbertonCourse {
 
   get name() { return this._name; }
 
-  set name(name) { this._name = name; }
+  set name(name) { if (typeof name !== 'string') throw TypeError('Name must be a string'); this._name = name; }
 
   get length() { return this._length; }
 
-  set length(length) { this._length = length; }
+  set length(length) { if (typeof length !== 'number') throw TypeError('Length must be a number'); this._length = length; }
 
   get students() { return this._students; }
 
-  set students(students) { this._students = students; }
+  set students(students) { if (!Array.isArray(students)) throw TypeError('Students must be an array'); this._students = students; }
 }
