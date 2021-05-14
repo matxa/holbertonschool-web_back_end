4d6c1d7e51dac5566a1abc775f160b42de042ef2
matxa <42968545+matxa@users.noreply.github.com>
2021-05-13 22:19:41 -0400

    5

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,11 +1,12 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
   }
 
   get sqft() { return this._sqft; }
 
-  set sqft(sqft) { this._sqft = sqft }
+  set sqft(sqft) { this._sqft = sqft; }
 }
-
-// stop 9:18pm May 12, 2021
