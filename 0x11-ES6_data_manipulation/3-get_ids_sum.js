03d14d8f1cf08b5792715baace5cf27e28d99049
matxa <42968545+matxa@users.noreply.github.com>
2021-05-17 11:32:07 -0400

    Reduce v.01

--- 0x11-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x11-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,5 @@
 import getListStudentIds from './1-get_list_student_ids';
 
 export default function getStudentIdsSum(getListStudents) {
-  if (getListStudents instanceof Array) {
-    return getListStudentIds(getListStudents).reduce((total, num) => total + num);
-  }
-  return 0;
+  return getListStudentIds(getListStudents).reduce((total, num) => total + num);
 }
