b07049cd69afe3b3be5b3cbc955590de7e2a729e
matxa <42968545+matxa@users.noreply.github.com>
2021-05-18 18:10:09 -0400

    100

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -3,10 +3,9 @@ export const weakMap = new WeakMap();
 export function queryAPI(endPoint) {
   if (weakMap.has(endPoint) === false) {
     weakMap.set(endPoint, 1);
+  } else if (weakMap.get(endPoint) >= 5) {
+    throw new Error('Endpoint load is high');
   } else {
-    if (weakMap.get(endPoint) >= 5) {
-      throw new Error('Endpoint load is high');
-    }
     weakMap.set(endPoint, weakMap.get(endPoint) + 1);
   }
 }
