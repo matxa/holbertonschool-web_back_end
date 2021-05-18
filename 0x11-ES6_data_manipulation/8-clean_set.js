ac8bf890ce7f74cb9bd25b9a4946575649523ffb
matxa <42968545+matxa@users.noreply.github.com>
2021-05-18 17:11:22 -0400

    check for startString length

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   let string = '';
   set.forEach((setString) => {
-    if (setString.startsWith(startString) && startString !== '') {
+    if (setString.startsWith(startString) && startString.length > 0) {
       const [, newString] = setString.split(startString);
       if (string.length < 1) {
         string = newString;
