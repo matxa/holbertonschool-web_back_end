e182bb7d3f6ebebfcb28624d0afd919729e5082b
matxa <42968545+matxa@users.noreply.github.com>
2021-05-18 17:16:18 -0400

    check for parameter types

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,17 @@
 export default function cleanSet(set, startString) {
-  let string = '';
-  set.forEach((setString) => {
-    if (setString.startsWith(startString) && startString.length > 0) {
-      const [, newString] = setString.split(startString);
-      if (string.length < 1) {
-        string = newString;
-      } else {
-        string = `${string}-${newString}`;
+  if (set instanceof Set && typeof startString === 'string') {
+    let string = '';
+    set.forEach((setString) => {
+      if (setString.startsWith(startString) && startString.length > 0) {
+        const [, newString] = setString.split(startString);
+        if (string.length < 1) {
+          string = newString;
+        } else {
+          string = `${string}-${newString}`;
+        }
       }
-    }
-  });
-  return string;
+    });
+    return string;
+  }
+  throw new Error('< set > should be a Set && < startString > should be a string');
 }
