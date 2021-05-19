db19a3ce58923bcd7c569edf51c62750269c45a7
matxa <42968545+matxa@users.noreply.github.com>
2021-05-18 23:40:07 -0400

    8

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -2,12 +2,8 @@ export default function cleanSet(set, startString) {
   if (startString === '') return '';
   try {
     const arr = [...set];
-    return arr.map((word) => {
-      if (word.startsWith(startString) && typeof word === 'string') {
-        return word.split(startString)[1];
-      }
-      return undefined;
-    }).filter((word) => word !== undefined).join('-');
+    return arr.filter((word) => word.startsWith(startString) && typeof word === 'string')
+      .map((word) => word.split(startString)[1]).join('-');
   } catch (error) {
     return '';
   }
