0cc0b8b7e0bca0033d5d160bf172cc699ef6911f
matxa <42968545+matxa@users.noreply.github.com>
2021-05-18 23:37:07 -0400

    8

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startString) {
   try {
     const arr = [...set];
     return arr.map((word) => {
-      if (word.startsWith(startString) && startString.length > 0) {
+      if (word.startsWith(startString) && typeof word === 'string') {
         return word.split(startString)[1];
       }
       return undefined;
