dac23ef977ac8d125c2502920361b964bccedaed
matxa <42968545+matxa@users.noreply.github.com>
2021-05-18 23:19:21 -0400

    8

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (set instanceof Set && typeof startString === 'string') {
-    let string = '';
-    set.forEach((setString) => {
-      if (setString.startsWith(startString) && startString.length > 0) {
-        const [, newString] = setString.split(startString);
-        if (string.length < 1) {
-          string = newString;
-        } else {
-          string = `${string}-${newString}`;
-        }
-      }
-    });
-    return string;
-  }
-  throw new Error('< set > should be a Set && < startString > should be a string');
+  const arr = [...set];
+  return arr.map((word) => {
+    if (word.startsWith(startString) && startString.length > 0) {
+      return word.split(startString)[1];
+    }
+    return undefined;
+  }).filter((word) => word !== undefined).join('-');
 }
