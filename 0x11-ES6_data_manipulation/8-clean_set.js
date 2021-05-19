638890e51c0ad5a50d5da24583d9fb56d0f5fd12
matxa <42968545+matxa@users.noreply.github.com>
2021-05-18 23:57:42 -0400

    8

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,13 @@
-// CleanSet turn set to array then to string
+// Given a set of strings, removes startString from every string in the set
 export default function cleanSet(set, startString) {
-  if (startString === '') {
-    return '';
-  }
+  if (startString === '') return '';
+
   try {
     return [...set]
-      .filter((word) => word.startsWith(startString) && typeof word === 'string')
-      .map((word) => word.replace(startString, ''))
+      .filter((str) => typeof str === 'string' && str.startsWith(startString))
+      .map((str) => str.replace(startString, ''))
       .join('-');
-  } catch (error) {
+  } catch (e) {
     return '';
   }
 }
