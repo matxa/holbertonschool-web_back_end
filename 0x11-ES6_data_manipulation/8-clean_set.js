6e9760d9e05ea9b6affe4fc4a2607af781c5eee5
matxa <42968545+matxa@users.noreply.github.com>
2021-05-18 23:24:06 -0400

    8

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,13 @@
 export default function cleanSet(set, startString) {
-  const arr = [...set];
-  return arr.map((word) => {
-    if (word.startsWith(startString) && startString.length > 0) {
-      return word.split(startString)[1];
-    }
-    return undefined;
-  }).filter((word) => word !== undefined).join('-');
+  try {
+    const arr = [...set];
+    return arr.map((word) => {
+      if (word.startsWith(startString) && startString.length > 0) {
+        return word.split(startString)[1];
+      }
+      return undefined;
+    }).filter((word) => word !== undefined).join('-');
+  } catch (error) {
+    return '';
+  }
 }
