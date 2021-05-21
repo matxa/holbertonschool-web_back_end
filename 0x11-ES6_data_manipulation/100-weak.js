aec4dde16e869a38a68483b072146b14fcbc9118
matxa <42968545+matxa@users.noreply.github.com>
2021-05-21 07:01:46 -0400

    check for endpoint load

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endPoint) {
   if (weakMap.has(endPoint) === false) {
     weakMap.set(endPoint, 1);
-  } else if (weakMap.get(endPoint) >= 5) {
+  } else if (weakMap.get(endPoint) + 1 >= 5) {
     throw new Error('Endpoint load is high');
   } else {
     weakMap.set(endPoint, weakMap.get(endPoint) + 1);
