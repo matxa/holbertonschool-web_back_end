475d145beca306b715f6c449a8d115d495f5aafe
matxa <42968545+matxa@users.noreply.github.com>
2021-05-24 19:46:37 -0400

    4

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  if (req) { res.write('Hello Holberton School!'); res.end(); }
+});
+app.listen(1245);
+
+module.exports = app;
