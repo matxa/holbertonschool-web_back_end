f20047af9e098e43038cb34d450030d718e6f531
matxa <42968545+matxa@users.noreply.github.com>
2021-05-24 19:27:29 -0400

    3

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+module.exports = async function countStudents(path) {
+  return fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) { throw new Error('Cannot load the database'); }
+    const records = data.split('\n');
+    const SWE = {
+      name: 'SWE',
+      list: [],
+    };
+    const CS = {
+      name: 'CS',
+      list: [],
+    };
+    const fields = [CS, SWE];
+    for (const i in records) {
+      if (i > 0) {
+        const [student, , , field] = records[i].split(',');
+        if (field === 'CS') {
+          CS.list.push(` ${student}`);
+        } else if (field === 'SWE') {
+          SWE.list.push(` ${student}`);
+        }
+      }
+    }
+    fields[0].list[0] = fields[0].list[0].trim();
+    fields[1].list[0] = fields[1].list[0].trim();
+    console.log(`Number of students: ${fields[0].list.length + fields[1].list.length}`);
+    console.log(`Number of students in ${fields[0].name}: ${fields[0].list.length}. List: ${fields[0].list}`);
+    console.log(`Number of students in ${fields[1].name}: ${fields[1].list.length}. List: ${fields[1].list}`);
+  });
+};
