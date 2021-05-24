b12ad37b09eabd1635d3ee9fb06802c2cd9cc4ae
matxa <42968545+matxa@users.noreply.github.com>
2021-05-24 18:32:36 -0400

    2

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -26,6 +26,8 @@ module.exports = function countStudents(path) {
         }
       }
     }
+    fields[0].list[0].trim();
+    fields[1].list[0].trim();
     console.log(`Number of students: ${fields[0].list.length + fields[1].list.length}`);
     console.log(`Number of students in ${fields[0].name}: ${fields[0].list.length}. List: ${fields[0].list}`);
     console.log(`Number of students in ${fields[1].name}: ${fields[1].list.length}. List: ${fields[1].list}`);
