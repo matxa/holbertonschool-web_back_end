609bbde3333ca3951eea3db7eb39a2ed5b1b9bb1
matxa <42968545+matxa@users.noreply.github.com>
2021-05-25 11:23:48 -0400

    5

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,15 +1,21 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+const FILE = process.argv[2];
+
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.write('Hello Holberton School!');
     res.end();
   } else if (req.url === '/students') {
-    countStudents('database.csv')
+    countStudents(FILE)
       .then((data) => {
         res.write('This is the list of our students\n');
         res.write(data);
+        res.end();
+      }).catch(() => {
+        res.write('This is the list of our students\n');
+        res.write('Cannot load the database');
         res.end();
       });
   }
