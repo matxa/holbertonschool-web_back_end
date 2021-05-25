3de2d7b5d35ef3c89be4c6010345d1a0af04373a
matxa <42968545+matxa@users.noreply.github.com>
2021-05-25 11:26:40 -0400

    7

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -2,13 +2,20 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
+const FILE = process.argv[2];
 
 app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (_, res) => {
-  countStudents('database.csv').then((data) => res.send(`This is the list of our students\n${data}`));
+  countStudents(FILE)
+    .then((data) => res.send(`This is the list of our students\n${data}`))
+    .catch(() => {
+      res.write('This is the list of our students\n');
+      res.write('Cannot load the database');
+      res.end();
+    });
 });
 
 app.listen(1245);
