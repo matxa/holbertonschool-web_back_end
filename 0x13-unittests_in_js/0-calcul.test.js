bf806518fe296cf320e61d3b37a3c1d65b35e838
matxa <42968545+matxa@users.noreply.github.com>
2021-05-26 23:33:01 -0400

    0

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -14,4 +14,10 @@ describe('calculateNumber', () => {
   it('(1.5, 3.7) should return -> 6', () => {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
+  it('(1.4, 4.5) should return -> 6', () => {
+    assert.equal(calculateNumber(1.4, 4.5), 6);
+  });
+  it('(1.4, 0) should return -> 1', () => {
+    assert.equal(calculateNumber(1.4, 0), 1);
+  });
 });
