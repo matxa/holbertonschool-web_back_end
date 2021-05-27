a788042afaceead0ff27541caa98ea4868faba84
matxa <42968545+matxa@users.noreply.github.com>
2021-05-26 23:35:16 -0400

    0

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -2,22 +2,22 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('(1, 3) should return -> 4', () => {
+  it('...', () => {
     assert.equal(calculateNumber(1, 3), 4);
   });
-  it('(1, 3.7) should return -> 5', () => {
+  it('...', () => {
     assert.equal(calculateNumber(1, 3.7), 5);
   });
-  it('(1.2, 3.7) should return -> 5', () => {
+  it('...', () => {
     assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-  it('(1.5, 3.7) should return -> 6', () => {
+  it('...', () => {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
-  it('(1.4, 4.5) should return -> 6', () => {
+  it('...', () => {
     assert.equal(calculateNumber(1.4, 4.5), 6);
   });
-  it('(1.4, 0) should return -> 1', () => {
+  it('...', () => {
     assert.equal(calculateNumber(1.4, 0), 1);
   });
 });
