1b7aa1eb3175956e6f8d1d6c4fd0abee6de8be21
matxa <42968545+matxa@users.noreply.github.com>
2021-05-27 21:54:00 -0400

    2

---
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,73 @@
+const chai = require('chai');
+const calculateNumber = require('./1-calcul');
+
+const { expect } = chai;
+
+describe('calculateNumber', () => {
+  it('...', () => {
+    expect(calculateNumber('SUM', 1, 3)).to.be.equal(4);
+  });
+  it('...', () => {
+    expect(calculateNumber('SUM', 1, 3.7)).to.be.equal(5);
+  });
+  it('...', () => {
+    expect(calculateNumber('SUM', 1.2, 3.7)).to.be.equal(5);
+  });
+  it('...', () => {
+    expect(calculateNumber('SUM', 1.5, 3.7)).to.be.equal(6);
+  });
+  it('...', () => {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.be.equal(6);
+  });
+  it('...', () => {
+    expect(calculateNumber('SUM', 1.4, 0)).to.be.equal(1);
+  });
+  it('...', () => {
+    expect(calculateNumber('SUM', 0, 1.4)).to.be.equal(1);
+  });
+});
+
+describe('calculateNumber', () => {
+  it('...', () => {
+    expect(calculateNumber('SUBTRACT', 1, 3)).to.be.equal(-2);
+  });
+  it('...', () => {
+    expect(calculateNumber('SUBTRACT', 1, 3.7)).to.be.equal(-3);
+  });
+  it('...', () => {
+    expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.be.equal(-3);
+  });
+  it('...', () => {
+    expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.be.equal(-2);
+  });
+  it('...', () => {
+    expect(calculateNumber('SUBTRACT', 4.5, 1.5)).to.be.equal(3);
+  });
+  it('...', () => {
+    expect(calculateNumber('SUBTRACT', 1.4, 0)).to.be.equal(1);
+  });
+  it('...', () => {
+    expect(calculateNumber('SUBTRACT', 0, 1.4)).to.be.equal(-1);
+  });
+});
+
+describe('calculateNumber', () => {
+  it('...', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.equal(0.2);
+  });
+  it('...', () => {
+    expect(calculateNumber('DIVIDE', 3, 1)).to.be.equal(3);
+  });
+  it('...', () => {
+    expect(calculateNumber('DIVIDE', 1.5, 3.7)).to.be.equal(0.5);
+  });
+  it('...', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0.4)).to.be.equal('Error');
+  });
+  it('...', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.be.equal('Error');
+  });
+  it('...', () => {
+    expect(calculateNumber('DIVIDE', 0, 1.4)).to.be.equal(0);
+  });
+});
