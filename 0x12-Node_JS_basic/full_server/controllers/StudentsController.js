570736772496310c52c19dd3a512ef6b77de5274
matxa <42968545+matxa@users.noreply.github.com>
2021-05-28 17:56:38 -0400

    check for server output

--- 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,18 +1,10 @@
 import readDatabase from '../utils';
 
-const DB = process.argv[2];
-let _DB = null;
-if (DB) {
-  _DB = DB.replace('./', '');
-}
-
-const __DB = _DB == null ? 'database.csv' : _DB;
-
 export default class StudentsController {
   static getAllStudents(equest, response) {
-    readDatabase(__DB)
+    readDatabase(process.argv[2].replace('./', ''))
       .then((fields) => {
-        const output = `Number of students: ${fields[0].list.length + fields[1].list.length}\nNumber of students in ${fields[0].name}: ${fields[0].list.length}. List: ${fields[0].list}\nNumber of students in ${fields[1].name}: ${fields[1].list.length}. List: ${fields[1].list}`;
+        const output = `Number of students in ${fields[0].name}: ${fields[0].list.length}. List: ${fields[0].list}\nNumber of students in ${fields[1].name}: ${fields[1].list.length}. List: ${fields[1].list}`;
         response.status(200).send(`This is the list of our students\n${output}`);
       }).catch(() => {
         response.status(500).send('Cannot load the database');
@@ -22,7 +14,7 @@ export default class StudentsController {
   static getAllStudentsByMajor(request, response) {
     if (request.params.major) {
       if (request.params.major === 'CS' || request.params.major === 'SWE') {
-        readDatabase(__DB)
+        readDatabase(process.argv[2].replace('./', ''))
           .then((fields) => {
             if (request.params.major === 'CS') {
               response.status(200).send(`List: ${fields[0].list}`);
